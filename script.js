e7ea0df6665e3c49455cb2be64fbba7e13cb59ca
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-01-30 14:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+function myFunc() {
+	let orderedList = document.getElementByTagName("ol");
+	let insertedValue = document.getElementbyId("newTodoInput").value;
 
+	let createList = document.createElement("li");
+	li.textContent = insertedValue;
+	orderedList.appendChild(createList);
+}
